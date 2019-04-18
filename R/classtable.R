c12605e2c059674408f6a7d247d682414d9f5589
Josh Errickson <josh@errickson.net>
2019-04-18 12:38:18 -0400

    First pass at classtable

--- R/classtable.R
+++ R/classtable.R
@@ -1,3 +1,11 @@
-gp_classtable <- function(model, threshold) {
+gp_classtable <- function(model, threshold, ...) {
+  stopifnot(is(model, "glm"))
+  stopifnot(model$family$family == "binomial")
 
+  observed <- model$data[,as.character(attributes(model$terms)$variables[[2]])]
+  classified <- as.numeric(gp_classify(model, threshold))
+
+  table(Observed = factor(observed, levels = c(0,1)),
+        Classified = factor(classified, levels = c(0,1)),
+        ...)
 }

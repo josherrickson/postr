0f63cc8cb4e3e8b86e07dbdb788c2a4569c306d6
Josh Errickson <josh@errickson.net>
2019-04-25 14:43:48 -0400

    Add AUC to ROC plot

--- R/ROC.r
+++ R/ROC.r
@@ -2,11 +2,14 @@
 #'
 #' @param model A glm logistic model
 #' @param thresholds Some numeric threshold
+#' @param AUC Logical; should AUC be printed on the plot? Default TRUE.
 #'
 #' @return ggplot object
 #' @export
 #' @import ggplot2
-postr_ROC <- function(model, thresholds = seq(0, 1, by = .01)) {
+postr_ROC <- function(model,
+                      thresholds = seq(0, 1, by = .01),
+                      AUC = TRUE) {
   thresholds <- sort(unique(thresholds))
   if (min(thresholds) == 0) {
     thresholds <- thresholds[-1]
@@ -17,10 +20,24 @@ postr_ROC <- function(model, thresholds = seq(0, 1, by = .01)) {
   tpr <- c(1, postr_tpr(model, thresholds), 0)
   fpr <- c(1, postr_fpr(model, thresholds), 0)
   d <- data.frame(tpr = rev(tpr), fpr = rev(fpr))
-  ggplot(d, aes(x = fpr, y = tpr)) +
+  d <- unique(d) # Duplicate TNR/TPR slow down plotting
+
+  if (AUC) {
+    auc <- round(postr_AUC(model, thresholds), 3)
+  }
+
+  g <- ggplot(d, aes(x = fpr, y = tpr)) +
     geom_point() +
     geom_line() +
-    geom_segment(x = 0, y = 0, xend = 1, yend = 1, color = "grey")
+    geom_segment(x = 0, y = 0, xend = 1, yend = 1, color = "grey") +
+    xlab("False Positive Rate") +
+    ylab("True Positive Rate")
+  if (AUC) {
+    g <- g +
+      geom_label(aes(x = 1, y = .1, label = paste("AUC =", auc)),
+                 hjust = "inward")
+  }
+  g
 }
 
 #' @rdname postr_ROC

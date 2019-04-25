d939d80d936d10622e17f547638d0a782f197520
Josh Errickson <josh@errickson.net>
2019-04-25 14:09:57 -0400

    Tests against invalid thresholdS

--- tests/testthat/test.classify.R
+++ tests/testthat/test.classify.R
@@ -11,3 +11,9 @@ test_that("returned object is appropriate", {
 test_that("pr alias works", {
   r <- runif(1); expect_identical(postr_classify(m, r), pr_classify(m, r))
 })
+
+test_that("error on invalid threshold", {
+  expect_error(pr_classify(m, -1))
+  expect_error(pr_classify(m, 2))
+})
+

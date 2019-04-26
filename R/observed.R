38fc6cbd04b5e5fccd4133d786d59f2f95d16ffa
Josh Errickson <josh@errickson.net>
2019-04-26 09:57:25 -0400

    Function to extract observed values

---
+++ R/observed.R
@@ -0,0 +1,27 @@
+#' Return observed values
+#'
+#' @param model A support model
+#'
+#' @return Observed values
+#' @export
+postr_observed <- function(model) {
+  UseMethod("postr_observed")
+}
+
+#' @export
+postr_observed.default <- function(model) {
+  stop(paste0("Obtaining observed values not supported for class ", class(model), "."))
+}
+
+#' @export
+postr_observed.glm <- function(model) {
+  model$data[,as.character(attributes(model$terms)$variables[[2]])]
+}
+
+#' @export
+postr_observed.glmerMod <- function(model) {
+  model@frame[,1]
+}
+
+#' @export
+pr_observed <- postr_observed

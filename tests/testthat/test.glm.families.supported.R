2d7af6cf433d30869f268aa6e746ceb391c9d8ed
Josh Errickson <josh@errickson.net>
2019-04-26 10:56:17 -0400

    Testing for glmer family check

--- tests/testthat/test.glm.families.supported.R
+++ tests/testthat/test.glm.families.supported.R
@@ -1,9 +1,26 @@
 context("glm family test")
 
-m <- "abc"
-suppressWarnings(m$family <- 1)
-suppressWarnings(m$family$family <- "binomial")
-class(m) <- "glm"
+data(mtcars)
+m <- glm(vs ~ mpg, data = mtcars, family = binomial)
+
+test_that("matches correct family", {
+  expect_true(.glm.families.supported(m, "binomial"))
+})
+
+test_that("matches if family in list", {
+  expect_true(.glm.families.supported(m, "poisson", "binomial"))
+})
+
+test_that("failure to match", {
+  expect_error(.glm.families.supported(m, "poisson"),
+               "glm family must be poisson")
+  expect_error(.glm.families.supported(m, "poisson", "gamma"),
+               "glm family must be one of poisson, gamma")
+})
+
+library(lme4)
+m <- glmer(vs ~ mpg + (1|cyl), data = mtcars, family = binomial)
+
 
 test_that("matches correct family", {
   expect_true(.glm.families.supported(m, "binomial"))

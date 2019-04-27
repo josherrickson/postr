bd70f77cb3b0b49a6d48fd8d6504c0dbef7f4471
Josh Errickson <josh@errickson.net>
2019-04-27 08:19:01 -0400

    RVF plot

---
+++ R/rvfplot.R
@@ -0,0 +1,38 @@
+#' Residual vs Fitted plot
+#'
+#' @param model A model
+#' @param smoothedcurve Logical; should a smoothed curve be plotted? Default: False
+#'
+#' @return A ggplot object
+#' @export
+postr_rvfplot <- function(model, smoothedcurve = FALSE) {
+  UseMethod("postr_rvfplot")
+}
+
+#' @export
+postr_rvfplot.default <- function(model, smoothedcurve) {
+  stop(paste0("RVF plot not supported for class ", class(model), "."))
+}
+
+#' @export
+postr_rvfplot.lm <- function(model, smoothedcurve = FALSE) {
+  res <- residuals(model)
+  fit <- fitted(model)
+  d <- data.frame(res = res, fit = fit)
+  g <- ggplot(d, aes(x = fit, y = res)) +
+    geom_point() +
+    geom_hline(yintercept = 0) +
+    xlab("Fitted Values") +
+    ylab("Residuals")
+  if (smoothedcurve) {
+    g <- g + geom_smooth(method = "loess")
+  }
+  g
+}
+
+#' @export
+postr_rvfplot.glm <- postr_rvfplot.lm
+
+#' @rdname postr_rvfplot
+#' @export
+pr_rvfplot <- postr_rvfplot
---
+++ tests/testthat/test.rvfplot.R
@@ -0,0 +1,18 @@
+context("RVF plot")
+
+data(mtcars)
+m <- lm(vs ~ mpg, data = mtcars)
+
+test_that("returned object is appropriate", {
+  expect_is(postr_rvfplot(m), "gg")
+
+})
+
+test_that("pr aliases", {
+  expect_equal(postr_rvfplot(m), pr_rvfplot(m))
+  # gg objects (plots in general) aren't identical but are all.equal
+})
+
+test_that("Improper object", {
+  expect_error(pr_rvfplot(1, .5), "RVF plot not supported")
+})

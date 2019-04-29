48fb28fb8c4178f37977d3ed117ab1a9ea78d57d
Josh Errickson <josh@errickson.net>
2019-04-28 20:33:54 -0400

    Leverage function

---
+++ R/leverage.R
@@ -0,0 +1,23 @@
+#' Return leverage values
+#'
+#' @param model A supported model
+#'
+#' @return Leverage values
+#' @export
+postr_leverage <- function(model) {
+  UseMethod("postr_leverage")
+}
+
+#' @export
+postr_leverage.default <- function(model) {
+  stop(paste0("Obtaining observed values not supported for class ", class(model), "."))
+}
+
+#' @export
+postr_leverage.lm <- function(model) {
+  hatvalues(model)
+}
+
+#' @rdname postr_leverage
+#' @export
+pr_leverage <- postr_leverage

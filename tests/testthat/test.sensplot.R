27bb0de667008b2732e35fae44f6d3992c495101
Josh Errickson <josh@errickson.net>
2019-05-01 06:22:55 -0400

    Test for sens plot

---
+++ tests/testthat/test.sensplot.R
@@ -0,0 +1,20 @@
+context("Sensitivity plot")
+
+data(mtcars)
+m <- glm(vs ~ mpg, data = mtcars, family = binomial)
+
+test_that("returned object is appropriate", {
+  expect_is(postr_sensplot(m), "gg")
+
+})
+
+test_that("pr aliases", {
+  expect_equal(postr_sensplot(m), pr_sensplot(m))
+  # gg objects (plots in general) aren't identical but are all.equal
+})
+
+test_that("Improper object", {
+  expect_error(pr_sensplot(1, .5), "Sensitivity plot not supported")
+  m <- update(m, family = poisson)
+  expect_error(pr_sensplot(m, .5), "family must be")
+})

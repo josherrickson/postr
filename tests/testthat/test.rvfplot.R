4ddd0f9ea4db27fb2f12847d9a927035b8541577
Josh Errickson <josh@errickson.net>
2019-05-17 14:36:29 -0400

    Testing smoothed curves

--- tests/testthat/test.rvfplot.R
+++ tests/testthat/test.rvfplot.R
@@ -15,6 +15,14 @@ test_that("returned object is appropriate", {
   expect_is(postr_rvfplot(m), "gg")
 })
 
+test_that("smoothed curve argument", {
+  g <- postr_rvfplot(m)
+  g2 <- postr_rvfplot(m, smoothedcurve = TRUE)
+  expect_is(g2, "gg")
+  # The smoothed curve should add exactly one geom (layer).
+  expect_equal(length(g$layers) + 1, length(g2$layers))
+})
+
 test_that("pr aliases", {
   expect_equal(postr_rvfplot(m), pr_rvfplot(m))
   # gg objects (plots in general) aren't identical but are all.equal
